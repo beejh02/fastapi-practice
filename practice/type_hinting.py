515f52a13522bc419d5dce682f51eb8a71d148d8
joon0448@beejh02 <joon0448@naver.com>
2025-03-24 20:11:33 +0900

    [feat] typehinting 공부

---
+++ practice/type_hinting.py
@@ -0,0 +1,14 @@
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    
+    return full_name
+
+print(get_full_name("john", "doe"))
+
+
+
+def get_name_with_age(name, age):
+    name_with_age = name + " is this old: " + age
+    return name_with_age
+
+print(get_name_with_age("ddd", 22))
