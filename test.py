008983bca1fadb27bee3de57891e02c285d36079
joon0448@beejh02 <joon0448@naver.com>
2025-03-25 14:20:24 +0900

    [test] 메서드 테스트

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
-def my_function(a, b, c):
-    print(a, b, c)
+from enum import Enum
 
-data = {"a": 1, "b": 2, "c": 3}
-my_function(**data)  # 1 2 3 출력
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+@app.get("/")
+def main():
+    return {"hello world"}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
